be01d623c0ac8a16bdb87c584d368a117cf83291
admin <admin@DESKTOP-6OQIV0R>
2018-11-01 14:56:52 +0800

    add files

---
+++ Wpflight/Model/WelcomeModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpflight.Model
+{
+    public class WelcomeModel : ObservableObject
+    {
+        private string _introduction = string.Empty;
+
+        public string Introduction
+        {
+            get => _introduction;
+            set
+            {
+                _introduction = value;
+                RaisePropertyChanged(() => Introduction);
+            }
+        }
+    }
+}
---
+++ Wpflight/ViewModel/WelcomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpflight.Model;
+
+namespace Wpflight.ViewModel
+{
+    public class WelcomeViewModel : ViewModelBase
+    {
+        private WelcomeModel welcomModel;
+
+        public WelcomeViewModel()
+        {
+            welcomModel = new WelcomeModel() { Introduction = "Welcome to Mvvm light" };
+        }
+
+        public WelcomeModel WelcomModel { get => welcomModel; set => welcomModel = value; }
+    }
+}
